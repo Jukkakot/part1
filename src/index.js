0a9ff9d86bc49449a1c64aa5e5edab6a2ff73c60
Jukka Kotilainen <jukkakot@metropolia.fi>
2020-09-12 17:13:42 +0300

    Toinen muutos

--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+const Header = (props) => {
+    return (
+        <h1>{props.course}</h1>
+    )
+}
+const Part = (props) => {
+    return (
+        <div>
+            <p>{props.part} {props.exercises}</p>
+        </div>
+    )
+}
+const Content = (props) => {
+    return (
+        <div>
+        <Part part={props.part1} exercises = {props.exercises1} />
+        <Part part={props.part2} exercises = {props.exercises2} />
+        <Part part={props.part3} exercises = {props.exercises3} />
+        </div>
+    )
+}
 
+const Total = (props) => {
+    return (
+        <div>
+        <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+        </div>
+    )
+}
 const App = () => {
     const course = 'Half Stack application development'
     const part1 = 'Fundamentals of React'
@@ -10,55 +38,15 @@ const App = () => {
     const part3 = 'State of a component'
     const exercises3 = 14
 
-    const Header = (course) => {
-        return (
-            <h1>{course.course}</h1>
-        )
-    }
-
-    const Content = () => {
-        return (
-            <div>
-            <p> {part1} {exercises1} </p>
-            <p> {part2} {exercises2} </p>
-            <p> {part3} {exercises3} </p>
-            </div>
-        )
-    }
-
-    const Total = () => {
-        return (
-            <div>
-            <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-            </div>
-        )
-    }
+    
     return (
         <div>
             <Header course ={course}/>
-            <Content />
-            <Total />
+            <Content part1 = {part1} part2 = {part2} part3 = {part3}
+             exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3} />
+            <Total exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3} />
         </div>
     )
-   /*
-    return (
-      
-        < div > 
-            <h1>{course}</h1>
-             
-            <p>
-                {part1} {exercises1}
-            </p>
-            <p>
-                {part2} {exercises2}
-            </p>
-            <p>
-                {part3} {exercises3}
-            </p>
-            <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-        </div>
-    )
-    */
     
 }
 
