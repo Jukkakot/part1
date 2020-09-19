3090847a416a4c65980202c9bc35ff3b1c658082
Jukka Kotilainen <jukkakot@metropolia.fi>
2020-09-19 16:39:24 +0300

    1.13

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -36,24 +36,26 @@ const Statistics = (props) => {
   )
 }
 
-const App = () => {
-  // tallenna napit omaan tilaansa
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+
+const App = (props) => {
+
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [selected, setSelected] = useState(Math.floor(Math.random() * 6))
+  const [points,setPoints] = useState(Array.apply(null, new Array(anecdotes.length)).map(Number.prototype.valueOf,0))
 
-  const anecdotes = [
-    'If it hurts, do it more often',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-  ]
-  
   const headerText1 = "give feedback"
   const headerText2 = "statistics"
- 
   return (
     <div>
       <Header text = {headerText1}/>
@@ -62,11 +64,24 @@ const App = () => {
       <Button onClick={() => setBad(bad + 1)} text = {"bad"}/>
       <Header text = {headerText2}/>
       <Statistics  good ={good} neutral = {neutral} bad = {bad}/>
+
+
+      <h1>------------Anekdootit-------------</h1>
+      <Button onClick={() => setSelected(Math.floor(Math.random() * 6))} text = {"Next anecdote"}/>
+      <Button onClick={() =>{
+        const copy = [...points]
+        copy[selected] += 1
+        setPoints(copy)
+      } } text = {"Vote"}/>
+      <p>{props.anecdotes[selected]}</p>
+      <p>Has {points[selected]} votes</p>
+      
     </div>
     
   )
 }
 
-ReactDOM.render(<App />, 
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
   document.getElementById('root')
 )
