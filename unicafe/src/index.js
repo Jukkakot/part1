942556ab3a0cff576c702fd34daf6edf1191b60b
Jukka Kotilainen <jukkakot@metropolia.fi>
2020-09-19 15:33:27 +0300

    1.11

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -13,33 +13,44 @@ const Button = (props) => (
 )
 const StatisticsLine = (props) => {
   return (
-    <p>{props.text} {props.value}</p>
+    <tr><td>{props.text}</td><td>{props.value}</td></tr>
   )
 }
 const Statistics = (props) => {
-  if(props.good == 0 && props.neutral == 0 && props.bad == 0){
+  if(props.good === 0 && props.neutral === 0 && props.bad === 0){
     return(
       <p>No feedback given</p>
     )
   }
   return (
-  <div>
+  <table>
+    <tbody>
     <StatisticsLine text ={"good"} value = {props.good}/>
     <StatisticsLine text ={"neutral"} value = {props.neutral}/>
     <StatisticsLine text ={"bad"} value = {props.bad}/>
     <StatisticsLine text ={"all"} value = {props.bad + props.good + props.neutral}/>
     <StatisticsLine text ={"average"} value = {(props.good - props.bad)/(props.bad + props.good + props.neutral)}/>
     <StatisticsLine text ={"positive"} value = {props.good/((props.bad + props.good + props.neutral)) *100 +" %"}/>
-      </div>
+    </tbody>
+      </table>
   )
 }
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  ]
+  
   const headerText1 = "give feedback"
   const headerText2 = "statistics"
  
@@ -52,6 +63,7 @@ const App = () => {
       <Header text = {headerText2}/>
       <Statistics  good ={good} neutral = {neutral} bad = {bad}/>
     </div>
+    
   )
 }
 
